ee3b9bbefbefc5f8a9aef41e873fc0ae9529c486
Vadim Anufriev <mailbox@vaan.io>
2020-12-22 20:08:39 +0300

    ex 1.3 refined

--- cats/src/main/scala/io/vaan/cats/scalawithcats/Chapter_1_Introduction/Ex_1_3.scala
+++ cats/src/main/scala/io/vaan/cats/scalawithcats/Chapter_1_Introduction/Ex_1_3.scala
@@ -5,23 +5,35 @@ import io.vaan.datatype.Cat
 // https://www.scalawithcats.com/dist/scala-with-cats.html#exercise-printable-library
 object Ex_1_3 {
 
+  // typeclass
   trait Printable[A] {
     def format(a: A): String
   }
 
+  // interface object
   object Printable {
+    def format[A](input: A)(implicit p: Printable[A]): String =
+      p.format(input)
+
+    def print[A](input: A)(implicit p: Printable[A]): Unit =
+      println(p.format(input))
+  }
+
+  // instances
+  object PrintableInstances {
     implicit val stringPrintable: Printable[String] = (a: String) => a
     implicit val intPrintable: Printable[Int] = (a: Int) => a.toString
 
     implicit val catPrintable: Printable[Cat] = (a: Cat) => {
-      val name = stringPrintable.format(a.name)
-      val age = intPrintable.format(a.age)
-      val color = stringPrintable.format(a.color)
+      val name = Printable.format(a.name)
+      val age = Printable.format(a.age)
+      val color = Printable.format(a.color)
 
       s"$name is a $age year-old $color cat."
     }
   }
 
+  // interface syntax
   object PrintableSyntax {
     implicit class PrintableOps[A](a: A)(implicit p: Printable[A]) {
       def format: String = p.format(a)
--- cats/src/test/scala/io/vaan/cats/scalawithcats/Chapter_1_Introduction/Tests.scala
+++ cats/src/test/scala/io/vaan/cats/scalawithcats/Chapter_1_Introduction/Tests.scala
@@ -1,5 +1,7 @@
 package io.vaan.cats.scalawithcats.Chapter_1_Introduction
 
+import io.vaan.cats.scalawithcats.Chapter_1_Introduction.Ex_1_3.Printable
+import io.vaan.cats.scalawithcats.Chapter_1_Introduction.Ex_1_3.PrintableInstances._
 import io.vaan.cats.scalawithcats.Chapter_1_Introduction.Ex_1_3.PrintableSyntax._
 import io.vaan.datatype.Cat
 import org.scalatest.flatspec.AnyFlatSpec
@@ -7,7 +9,10 @@ import org.scalatest.flatspec.AnyFlatSpec
 class Tests extends AnyFlatSpec {
   it should "ex 1.3 printable library" in {
     val barsik = Cat("Barsik", 4, "white")
-    barsik.format == "Barsik is a 4 year-old white cat."
+    val expected = "Barsik is a 4 year-old white cat."
+
+    assert(barsik.format == expected)
+    assert(Printable.format(barsik) == expected)
 
     val stream = new java.io.ByteArrayOutputStream()
     Console.withOut(stream) {
